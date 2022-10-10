75b0337632aefba3c317a719d3bf14cc54c2b018
Thomas Truong <TruongThomasQ@gmail.com>
2022-10-10 02:22:31 -0700

    Refactored a lot of the code.

--- Assignment3/src/head.c
+++ Assignment3/src/head.c
@@ -9,6 +9,10 @@
 #include <stdio.h>
 #include <string.h>
 
+int charIsInt(char toCheck);
+char toLower(char c);
+int cArrayToInt(char *cArray);
+
 int main(int argc, char *argv[]) {
   char printFlag = 'n';    // default is n.
   int amountToPrint = 10;  // default is 10 lines.
@@ -18,92 +22,49 @@ int main(int argc, char *argv[]) {
 
   // Get arguments.
   for (int i = 1; i < argc; ++i) {
-    char *argv_i = argv[i];
+    char *argvI = argv[i];
     // Flag notation spotted.
-    if (argv_i[0] == '-') {
+    if (argvI[0] == '-') {
       // Size of ith argument (amount of char in argument).
-      int argv_i_size = strlen(argv_i);
+      int argvISize = strlen(argvI);
       // Multi-flag; go through every char in ith argument.
-      for (int j = 1; j < argv_i_size; ++j) {
+      for (int j = 1; j < argvISize; ++j) {
         // Set proper values based on flag(s).
-        switch (argv_i[j]) {
+        switch (argvI[j]) {
           case 'n':
           case 'N':
-          {
-            printFlag = 'n';
-            int value = 0;
-            // Get number if exists for -n flag.
-            if (j + 1 < argv_i_size) {
-              for (int k = j + 1, start = k; k < argv_i_size; ++k) {
-                value *= 10;
-                // Is not a number; print error and stop.
-                if (argv_i[k] < 48 || argv_i[k] > 57) {
-                  printf("head: invalid number of lines: \'");
-                  // Get the number of lines that caused an error.
-                  for (int e = start; e < argv_i_size; ++e) {
-                    printf("%c", argv_i[e]);
-                  }
-                  printf("\'\n");
-                  return -1;
-                }
-                value += argv_i[k] - 48;  // Convert ascii to regular int.
-              }
-              j = argv_i_size;
-            } else if (i + 1 < argc) {
-              // Space between -n flag and number.
-              char *nextArg = argv[i + 1];
-              int nextArg_size = strlen(nextArg);
-              for (int p = 0; p < nextArg_size; ++p) {
-                value *= 10;
-                // Is not a number; print error and stop.
-                if (nextArg[p] < 48 || nextArg[p] > 57) {
-                  printf("head: invalid number of lines: \'%s\'\n", nextArg);
-                  return -1;
-                }
-                value += nextArg[p] - 48;  // Convert ascii to regular int.
-                ++i;
-              }
-            }
-            amountToPrint = value;
-            break;
-          }
           case 'c':
           case 'C':
           {
-            printFlag = 'c';
-            int value = 0;
-            // Get number if exists for -n flag.
-            if (j + 1 < argv_i_size) {
-              for (int k = j + 1, start = k; k < argv_i_size; ++k) {
-                value *= 10;
-                // Is not a number; print error and stop.
-                if (argv_i[k] < 48 || argv_i[k] > 57) {
-                  printf("head: invalid number of bytes: \'");
-                  // Get the number of bytes that caused an error.
-                  for (int e = start; e < argv_i_size; ++e) {
-                    printf("%c", argv_i[e]);
-                  }
-                  printf("\'\n");
-                  return -1;
-                }
-                value += argv_i[k] - 48;  // Convert ascii to regular int.
-              }
-              j = argv_i_size;
-            } else if (i + 1 < argc) {
-              // Space between -n flag and number.
-              char *nextArg = argv[i + 1];
-              int nextArg_size = strlen(nextArg);
-              for (int p = 0; p < nextArg_size; ++p) {
-                value *= 10;
-                // Is not a number; print error and stop.
-                if (nextArg[p] < 48 || nextArg[p] > 57) {
-                  printf("head: invalid number of bytes: \'%s\'\n", nextArg);
-                  return -1;
-                }
-                value += nextArg[p] - 48;  // Convert ascii to regular int.
-                ++i;
-              }
+            // Set flag variable.
+            printFlag = toLower(argvI[j]);
+
+            // Determine where the line/byte number is.
+            char *cArray = NULL;
+            if (j + 1 < argvISize) {  // -flag#
+              cArray = &argvI[j + 1];
+            } else if (i + 1 < argc) {  // -flag #
+              cArray = argv[i + 1];
+              // Skip next argument since it is the line/byte number.
+              ++i;
+            } else {
+              printf("head: option requires an argument -- \'%c\'\n", argvI[j]);
+              return -1;
             }
+
+            int value = cArrayToInt(cArray);
+            // Not a valid int, print error msg and exit.
+            if (value == -1) {
+              printf("head: invalid number of ");
+              if (printFlag == 'c') {  // Bytes flag.
+                printf("bytes");
+              } else {  // Lines flag.
+                printf("lines");
+              }
+              printf(": \'%s\'\n", &argvI[j + 1]);
+              return -1;
+            }
+            j = argvISize;
             amountToPrint = value;
             break;
           }
@@ -116,13 +77,13 @@ int main(int argc, char *argv[]) {
             printHeaders = 1;
             break;
           default:
-            printf("head: invalid trailing option -- %c\n", argv_i[j]);
+            printf("head: invalid trailing option -- %c\n", argvI[j]);
             return -1;
         }
       }
     } else {
       // Not flag? Should be file names then.
-      fileNames[fileIndex] = argv_i;
+      fileNames[fileIndex] = argvI;
       ++fileIndex;
     }
   }
@@ -133,4 +94,29 @@ int main(int argc, char *argv[]) {
   }
 
   return 0;
+}
+
+int charIsInt(char toCheck) {
+  return toCheck > 47 && toCheck < 58;
+}
+
+char toLower(char c) {
+  if (c > 64 && c < 91) {
+    return c + 32;
+  }
+  return c;
+}
+
+int cArrayToInt(char *cArray) {
+  int value = 0;
+  int cArraySize = strlen(cArray);
+  for (int i = 0; i < cArraySize; ++i) {
+    value *= 10;
+    // Not an int.
+    if (!charIsInt(cArray[i])) {
+      return -1;
+    }
+    value += cArray[i] - 48;  // Convert ascii to regular int.
+  }
+  return value;
 }
