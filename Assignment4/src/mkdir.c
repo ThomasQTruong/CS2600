63fbe5a65aeeaa3083d14bd65892f045b6b03798
ThomasQTruong <TruongThomasQ@gmail.com>
2022-10-19 15:15:06 -0700

    Started the basic template for Assignment4.

---
+++ Assignment4/src/mkdir.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2022 [Thomas Truong]
+ * Bronco ID: 014426906
+ * CS2600, Fall 2022
+ * Programming Assignment 4
+ * Replication of mkdir UNIX command.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  
+
+  return 0;
+}
