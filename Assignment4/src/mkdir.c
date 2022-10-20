703be05cb976c16c5fd63cef26f63ad486cadec2
ThomasQTruong <TruongThomasQ@gmail.com>
2022-10-20 13:47:38 -0700

    Played around a bit, can create directories.

--- Assignment4/src/mkdir.c
+++ Assignment4/src/mkdir.c
@@ -7,9 +7,33 @@
  */
 
 #include <stdio.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int main(int argc, char *argv[]) {
-  
+  char *folderNames[argc - 1];
+  int folderIndex = 0;
+
+  // For every argument in argv.
+  for (int i = 1; i < argc; ++i) {
+    // If argument is a flag.
+    if ((argv[i])[0] == '-') {
+    } else {  // Not a flag, is folder name.
+      folderNames[folderIndex] = argv[i];
+      ++folderIndex;
+    }
+  }
+
+  for (int i = 0; i < folderIndex; ++i) {
+    DIR *dirPtr = opendir(folderNames[i]);
+    if (dirPtr == NULL) {
+      int createDir = mkdir(folderNames[i], S_IRWXU);
+    } else {
+      printf("Already exists!\n");
+    }
+  }
 
   return 0;
 }
