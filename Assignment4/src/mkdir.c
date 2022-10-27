4efd249eeaa3deacb01e450d315404f625da3bbb
ThomasQTruong <TruongThomasQ@gmail.com>
2022-10-27 15:44:52 -0700

    Created the -p and -v flag options. Need to test some more and maybe work on -m flag.

--- Assignment4/src/mkdir.c
+++ Assignment4/src/mkdir.c
@@ -7,6 +7,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -14,17 +15,35 @@
 #include <errno.h>
 #include <string.h>
 
+// Counts the amount of times a char appears in string.
 int countCharInString(const char *s, const char target);
 
 int main(int argc, char *argv[]) {
-  char *dirNames[argc - 1];
-  int dirIndex = 0;
-  int pFlag = 0;
+  char *dirNames[argc - 1];   // Array that contains the directory names.
+  int dirIndex = 0;           // Index in array, dirNames.
+  int pFlag = 0;              // p flag (-p, parents).
+  int vFlag = 0;              // v flag (-v, verbose).
 
-  // For every argument in argv.
+  // For every argument in argv (Check for flags/names).
   for (int i = 1; i < argc; ++i) {
+    char *argvI = argv[i];
     // If argument is a flag.
-    if ((argv[i])[0] == '-') {
+    if (argvI[0] == '-') {
+      int argvISize = strlen(argvI);
+      for (int j = 0; j < argvISize; ++j) {
+        switch (argvI[j]) {
+          // p flag (parents).
+          case 'p':
+          case 'P':
+            pFlag = 1;
+          break;
+          // v flag (verbose).
+          case 'v':
+          case 'V':
+            vFlag = 1;
+          break;
+        }
+      }
     } else {  // Not a flag, is folder name.
       dirNames[dirIndex] = argv[i];
       ++dirIndex;
@@ -35,32 +54,57 @@ int main(int argc, char *argv[]) {
   for (int i = 0; i < dirIndex; ++i) {
     int createDir = mkdir(dirNames[i], S_IRWXU);
     // An error occured when creating directory.
-    if (errno != 0) {
+    if (errno != 0 && pFlag == 0) {
       // Print error message.
       printf("mkdir: cannot create directory \'%s\': %s\n",
               dirNames[i], strerror(errno));
+    } else if (errno == 0 && vFlag == 1) {
+      // Error did not occur and verbose flag is true.
+      printf("mkdir: created directory \'%s\'\n", dirNames[i]);
     }
-    printf("%i\n", errno);
-    if (errno == 2) {  // -p flag used.
-      // Recursively create directory(s) if needed.
-      // int dirSize = sizeof(dirNames[i]) / sizeof(char);
-      // for (int j = 0; j < dirSize; ++j) {
 
-      // }
-      char *currentStr = dirNames[i];
-      char *remainder = NULL;
-      do {
-        strtok_r(currentStr, "/", &remainder);
-        printf("%s\n", currentStr);
-        mkdir(currentStr, S_IRWXU);
-        currentStr = remainder;
-      } while ((countCharInString(remainder, '/') > 0));
+    // No parent(s) error and -p flag used.
+    if (errno == 2 && pFlag == 1) {
+      // Create directory(s) if needed.
+      // Dynamically allocate space to fullPath and currentPath.
+      char *fullPath = dirNames[i];
+      char *currentPath = (char*)calloc(strlen(dirNames[i]) + 1, sizeof(char));
+
+      // Stores the dir extracted.
+      char *bufferStr = NULL;
+
+      // Amount of times to repeat loop.
+      int repeatAmount = countCharInString(fullPath, '/');
+
+      // Create every folder needed.
+      while (repeatAmount >= 0) {
+        bufferStr = strtok_r(fullPath, "/", &fullPath);
+        strcat(currentPath, bufferStr);
+        strcat(currentPath, "/");
+        int success = mkdir(currentPath, S_IRWXU);
+        if (success == 0 && vFlag == 1) {  // Succeded (-1 is fail).
+          printf("mkdir: created directory \'%s\'\n", currentPath);
+        }
+        repeatAmount--;
+      }
+
+      // Free the dynamically allocated memory.
+      free(currentPath);
     }
   }
 
   return 0;
 }
 
+
+
+/**
+ * Counts the amount of times a char appears in string.
+ * 
+ * @param s - the string to check.
+ * @param target - the char to count.
+ * @return int - the count.
+ */
 int countCharInString(const char *s, const char target) {
   int count = 0;
   for (int i = 0; s[i] != '\0'; ++i) {
