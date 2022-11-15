269ba152f01c8aa164fb083b572a658d02f60f0d
ThomasQTruong <TruongThomasQ@gmail.com>
2022-11-15 13:18:15 -0800

    Implemented the starting code for timeout.c

---
+++ Assignment5/src/timeout.c
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2022 [Thomas Truong]
+ * Bronco ID: 014426906
+ * CS2600, Fall 2022
+ * Programming Assignment 5
+ * Timeout.c - Add more to this.
+ */
+
+int main(void) {
+
+}
