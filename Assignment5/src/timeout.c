bdd85a3a57191c31b74d71c935fdd81efb7a7f2c
ThomasQTruong <TruongThomasQ@gmail.com>
2022-11-17 15:25:35 -0800

    Created struct Data to obtain command-line arguments and clean up mean.

--- Assignment5/src/timeout.c
+++ Assignment5/src/timeout.c
@@ -12,92 +12,30 @@
 #include <time.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int isInt(char *toCheck);
 int stringToInt(char *toConvert);
+struct Data getData(int argc, char *argv[]);
+
+struct Data {
+  int timeoutSeconds;  // Seconds till timeout (default: 10).
+  char *fileName;      // Name of file.
+  int verbose;         // Verbose flag.
+};
+
 
 int main(int argc, char *argv[]) {
-  int timeoutSeconds = 10;  // Seconds till timeout (default: 10).
-  char *fileName = NULL;    // Name of file.
-  int verbose = 0;          // Verbose flag.
-
-  // For every argument (skip ./timeout).
-  for (int i = 1; i < argc; ++i) {
-    char* argvi = argv[i];
-
-    // Possible flag.
-    if (argvi[0] == '-') {
-      int argviSize = strlen(argvi);  // Get size.
-
-      // For every character in flag.
-      for (int j = 1; j < argviSize; ++j) {
-        switch (argvi[j]) {
-          // Verbose flag.
-          case 'v':
-          case 'V':
-            verbose = 1;
-            break;
-          // Timeout seconds flag.
-          case 'T':
-          case 't':
-            // N on same index (-tN).
-            if (j + 1 < argviSize) {
-              // N is an int, store in timeoutSeconds.
-              if (isInt(&(argvi[j + 1])) == 1) {
-                timeoutSeconds = stringToInt(&(argvi[j + 1]));
-                j = argviSize;
-
-              // ERROR: Time is invalid.
-              } else {
-                printf("timeout: invalid number of seconds: \'%s\'\n",
-                        &(argvi[j + 1]));
-                return -1;
-              }
-
-            // N should be on the next element (-t N).
-            } else if (i + 1 < argc) {
-              // If the next argument is an int.
-              if (isInt(argv[i + 1]) == 1) {
-                timeoutSeconds = stringToInt(argv[i + 1]);
-                ++i;  // Skip next argument since it was used for time.
-              } else {  // ERROR: Time is invalid.
-                printf("timeout: invalid number of seconds: \'%s\'\n",
-                        argv[i + 1]);
-                return -1;
-              }
-
-            // No other argment was given. (-t)
-            } else {
-              printf("timeout: option requires an argument --\'%c\'\n",
-                      argvi[1]);
-              return -1;
-            }
-            break;
-          // Invalid flag.
-          default:
-            printf("timeout: invalid option -- \'%c\'\n", argvi[j]);
-            return 1;
-        }
-      }
-    } else {
-      // Not a flag, should be file name (only keep latest file).
-      fileName = argv[i];
-    }
-  }
-
-  // Obtained every information needed; begin program.
-  if (fileName == NULL) {  // No file given.
-    printf("timeout: no file given\n");
-    return -1;
-  }
+  // Obtain data from the command-line arguments.
+  struct Data data = getData(argc, argv);
 
   // Create stat buffer and obtain the stats of fileName.
   struct stat sb;
-  int success = stat(fileName, &sb);
+  int success = stat(data.fileName, &sb);
   // File not found.
   if (success == -1) {
     printf("timeout: file not found\n");
-    return 1;
+    exit(1);
   }
 
   // Get process ID.
@@ -107,19 +45,19 @@ int main(int argc, char *argv[]) {
   time_t modificationTime = sb.st_mtime;
 
   // Sleep timeoutSeconds times.
-  for (int counter = 0; counter < timeoutSeconds; ++counter) {
+  for (int counter = 0; counter < data.timeoutSeconds; ++counter) {
     sleep(1);
 
     // Reobtain file stats.
-    success = stat(fileName, &sb);
+    success = stat(data.fileName, &sb);
     // File not found.
     if (success == -1) {
       printf("timeout: file not found\n");
-      return 1;
+      exit(1);
     }
 
     // Print time and counter if verbose flag is used.
-    if (verbose == 1) {
+    if (data.verbose == 1) {
       printf("%sCounter: %i\n", ctime(&modificationTime), counter);
     }
 
@@ -167,4 +105,81 @@ int stringToInt(char *toConvert) {
   }
 
   return converted;
+}
+
+
+struct Data getData(int argc, char *argv[]) {
+  struct Data data;
+
+  // For every argument (skip ./timeout).
+  for (int i = 1; i < argc; ++i) {
+    char* argvi = argv[i];
+
+    // Possible flag.
+    if (argvi[0] == '-') {
+      int argviSize = strlen(argvi);  // Get size.
+
+      // For every character in flag.
+      for (int j = 1; j < argviSize; ++j) {
+        switch (argvi[j]) {
+          // Verbose flag.
+          case 'v':
+          case 'V':
+            data.verbose = 1;
+            break;
+          // Timeout seconds flag.
+          case 'T':
+          case 't':
+            // N on same index (-tN).
+            if (j + 1 < argviSize) {
+              // N is an int, store in timeoutSeconds.
+              if (isInt(&(argvi[j + 1])) == 1) {
+                data.timeoutSeconds = stringToInt(&(argvi[j + 1]));
+                j = argviSize;
+
+              // ERROR: Time is invalid.
+              } else {
+                printf("timeout: invalid number of seconds: \'%s\'\n",
+                        &(argvi[j + 1]));
+                exit(1);
+              }
+
+            // N should be on the next element (-t N).
+            } else if (i + 1 < argc) {
+              // If the next argument is an int.
+              if (isInt(argv[i + 1]) == 1) {
+                data.timeoutSeconds = stringToInt(argv[i + 1]);
+                ++i;  // Skip next argument since it was used for time.
+              } else {  // ERROR: Time is invalid.
+                printf("timeout: invalid number of seconds: \'%s\'\n",
+                        argv[i + 1]);
+                exit(1);
+              }
+
+            // No other argment was given. (-t)
+            } else {
+              printf("timeout: option requires an argument --\'%c\'\n",
+                      argvi[1]);
+              exit(1);
+            }
+            break;
+          // Invalid flag.
+          default:
+            printf("timeout: invalid option -- \'%c\'\n", argvi[j]);
+            exit(1);
+        }
+      }
+    } else {
+      // Not a flag, should be file name (only keep latest file).
+      data.fileName = argv[i];
+    }
+  }
+
+  // Obtained every information needed; begin program.
+  if (data.fileName == NULL) {  // No file given.
+    printf("timeout: no file given\n");
+    exit(1);
+  }
+
+  return data;
 }
