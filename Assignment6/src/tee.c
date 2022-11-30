726dc3fe378bf9e8e1c3a14f2a94ae9e85d35016
ThomasQTruong <TruongThomasQ@gmail.com>
2022-11-30 15:18:44 -0800

    Created the basic file structure for Assignment6.

---
+++ Assignment6/src/tee.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2022 [Thomas Truong]
+ * Bronco ID: 014426906
+ * CS2600, Fall 2022
+ * Programming Assignment 6
+ * Tee.c - 
+ */
+
+int main(int argc, char *argv[]) {
+  
+
+  return 0;
+}
